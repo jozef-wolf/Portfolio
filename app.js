8bb109430e59b3ddb9a5831f6cad0595cc4a73f0
jozef-wolf <jozefwolf@o2.pl>
2020-12-21 21:03:28 +0100

    parallax effect ver 1

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+/*Parallax scrolling effect*/
+
+let bg = document.getElementById("bg");
+let earth = document.getElementById("earth");
+let dune = document.getElementById("dune");
+let text = document.getElementById("text");
+
+window.addEventListener('scroll', function () {
+    var value = window.scrollY;
+
+    bg.style.top = value * 0.5 + 'px';
+    earth.style.left = -value * 0.5 + 'px';
+    earth.style.top = -value * 0.15 + 'px';
+    dune.style.top = value * 0.15 + 'px';
+    text.style.top = value * 1 + 'px';
+
+})
