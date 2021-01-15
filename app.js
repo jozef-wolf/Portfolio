0d9044d80006664a6b10349cbcfdafbe2a8e8656
Jozef Wolf <jozefwolf@o2.pl>
2021-01-15 13:21:52 +0100

    wip - sidenav js

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 
+// const openNav = document.querySelector('.menu-icon');
+// openNav.addEventListener('hover', () => {
+//     document.getElementById("mySidenav").style.height = "10%";
+// }
+
+// })
+/* Open the sidenav */
 function openNav() {
     document.getElementById("mySidenav").style.height = "10%";
 }
 
 /* Close/hide the sidenav */
+// const closeNav = document.querySelector('')
 function closeNav() {
     document.getElementById("mySidenav").style.height = "0";
 }
@@ -16,12 +24,12 @@ let mountains = document.getElementById("mountains");
 let text = document.getElementById("text");
 
 window.addEventListener('scroll', function () {
-    var value = window.scrollY;
+    let value = window.scrollY;
 
     bg.style.top = value * 0.5 + 'px';
     earth.style.left = -value * 0.5 + 'px';
     earth.style.top = -value * 0.15 + 'px';
-    mountains.style.top = value * 0.15 + 'px';
-    text.style.top = value * 1 + 'px';
+    mountains.style.top = value * 0.40 + 'px';
+    text.style.top = value * 1.1 + 'px';
 
 })
